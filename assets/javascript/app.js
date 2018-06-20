b2a6a43fb6eaa5f21f04d49a799698f5e40d84ac
Cody Sherrod <w.cody.sherrod@gmail.com>
2018-06-19 21:11:30 -0500

    minor style changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,76 +1,80 @@
-$(document).ready(function() {
+$(function () {
+  var topics = [
+    'Aresenal',
+    'Aston Villa',
+    'Bournemouth',
+    'Burnley',
+    'Chelsea',
+    'Crystal Palace',
+    'Everton',
+    'Fulham',
+    'Hull City',
+    'Leicester City',
+    'Liverpool',
+    'Manchester City',
+    'Manchester United',
+    'New Castle United',
+    'Stoke City',
+    'Tottenham Hotspur'
+  ]
+  var searchValue
 
-var topics = [
-    "Aresenal", "Aston Villa", "Bournemouth", "Burnley", "Chelsea", "Crystal Palace",
-    "Everton", "Fulham", "Hull City", "Leicester City", "Liverpool", "Manchester City",
-    "Manchester United", "New Castle United", "Stoke City", "Tottenham Hotspur"];
-var searchValue;
-
-//make sure buttons have data attributes
-$(document).ready(function(buildButton) {
-    for (var i=0; i< topics.length; i++) {
-        var topicButtons = $("<button class='givenTopics btn btn-outline-secondary'>");
-        topicButtons.attr("data-topics", topics[i]);
-        topicButtons.text(topics[i]);
-        $(".buttonHolder").append(topicButtons);
+  // make sure buttons have data attributes
+  $(document).ready(function (buildButton) {
+    for (var i = 0; i < topics.length; i++) {
+      var topicButtons = $(
+        "<button class='givenTopics btn btn-outline-secondary'>"
+      )
+      topicButtons.attr('data-topics', topics[i])
+      topicButtons.text(topics[i])
+      $('.buttonHolder').append(topicButtons)
     }
-});
+  })
 
+  // function buildQueryURL() {
+  $('#searchSubmit').on('click', function (event) {
+    event.preventDefault()
+    var searchValue = $('#searchSubmit')
+      .val()
+      .trim()
+    var newButtons = $("<button class='givenTopics btn btn-outline-secondary'>")
+    newButtons.attr('data-topics', searchValue)
+    newButtons.text(searchValue)
+    $('.buttonHolder').append(newButtons)
+  })
 
-
-// function buildQueryURL() {
-$("#search").on("click", function(event) {
-    event.preventDefault();
-    var searchValue = $("#inlineFormInputName2")
-    .val()
-    .trim();
-    var newButtons = $("<button class='givenTopics btn btn-outline-secondary'>");
-    newButtons.attr("data-topics", searchValue);
-    newButtons.text(searchValue);
-    $(".buttonHolder").append(newButtons);
-});
-
-$("button").on("click", "data-topics", function() {
-    console.log("hey");
-    console.log(this);
-    var searchValue = $(this).attr("data-topics");
-    console.log(searchValue);
+  $('.button').on('click', 'data-topics', function () {
+    console.log('hey')
+    console.log(this)
+    var searchValue = $(this).attr('data-topics')
+    console.log(searchValue)
     var queryURL =
-    "https://api.giphy.com/v1/gifs/search?api_key=2gmU5dW54vDq1BJNHss3h7Mc4lgz12P2&q=" +
-    searchValue +
-    "&limit=10&offset=0&rating=PG-13&lang=en";
+      'https://api.giphy.com/v1/gifs/search?api_key=2gmU5dW54vDq1BJNHss3h7Mc4lgz12P2&q=' +
+      searchValue +
+      '&limit=10&offset=0&rating=PG-13&lang=en'
 
     $.ajax({
-    url: queryURL,
-    method: "GET"
-    }).then(function(response) {
-    var results = response.data;
-    for (var i= 0; i< results.length; i++) {
-    var image = $("<img class='gifs'>");
-    image.attr("src", results[i].images.fixed_height_still.url);
-    image.attr("data-gif", results[i].images.fixed_height.url);
-    image.attr("data-still", results[i].images.fixed_height_still.url);
-    var rating = $("<p>" + results[i].rating + "</p>");
-    var gifInfo = $("<div class='gifDiv'>");
-    gifInfo.append(image);
-    gifInfo.append(rating);
-    $("#gifResults").append(gifInfo);
-    //   $(".givenTopics").append(topicButtons);
-    }
+      url: queryURL,
+      method: 'GET'
+    }).then(function (response) {
+      var results = response.data
+      for (var i = 0; i < results.length; i++) {
+        var image = $("<img class='gifs'>")
+        image.attr('src', results[i].images.fixed_height_still.url)
+        image.attr('data-gif', results[i].images.fixed_height.url)
+        image.attr('data-still', results[i].images.fixed_height_still.url)
+        var rating = $('<p>' + results[i].rating + '</p>')
+        var gifInfo = $("<div class='gifDiv'>")
+        gifInfo.append(image)
+        gifInfo.append(rating)
+        $('#gifResults').append(gifInfo)
+        //   $(".givenTopics").append(topicButtons);
+      }
+    })
+  })
 
+  // $(".buttonHolder").on("click", function(event) {
+  //     console.log("dang");
+  // });
 
-    });
-
-});
-
-// $(".buttonHolder").on("click", function(event) {
-//     console.log("dang");
-// });
-
-});
-
-// function clear() {
-//     $("#buttonHolder").empty();
-// }
-
-
+}
